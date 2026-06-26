ec91c59de4558615da314f4688cf89c65e656288
Web Developer <web-developer@lacasetta.com>
2026-06-26 05:12:53 +0000

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/La-Casetta-website/',
   plugins: [react()],
   server: {
     port: 3000,
